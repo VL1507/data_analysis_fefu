9903c9bac44188fb2b9bb618a80616402b0f2a1e
VL1507 <vova.mashyanov@gmail.com>
2025-12-28 04:16:00 +1000

    changed echo from True to False

--- data_generator/database.py
+++ data_generator/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from config import DATABASE_URL
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 
 Session = sessionmaker(bind=engine)
